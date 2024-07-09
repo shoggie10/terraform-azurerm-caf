fb6aee62c635be52ec0cc5a5a62cb54c163611ed
shoggie10 <shoggie10@gmail.com>
2024-07-09 09:38:30 -0500

    Update lb.tf

--- lb.tf
+++ lb.tf
@@ -19,30 +19,6 @@ output "lb" {
   value = module.lb
 }
 ==========||========================
-Granular Agent Pool Permissions: While Terraform can manage agent pools, it does not offer detailed permissions settings for individual agent pools.
-
-Release Pipeline Configuration: Terraform supports build pipelines but does not fully support the management of release pipelines, which are crucial for continuous deployment scenarios.
-
-Service Connections Detailed Management: The provider supports creating service connections but lacks the ability to manage detailed settings and security for each type of service connection (like Kubernetes service connections, for instance).
-
-Repository Webhooks: Management of webhooks for repositories is not currently supported, which are often used for triggering CI/CD processes in response to repo events.
-
-Wiki Management: There is no support for managing project wikis, including permissions and content.
-
-Test Plans and Test Suites: Terraform does not manage Azure DevOps Test Plans and Test Suites, which are used for managing manual and automated tests.
-
-Dashboard and Widget Configuration: While project and team configuration is supported, detailed dashboard and widget configurations within those are not.
-
-Notification Configuration: Management of project-level notifications or personal notifications settings is not supported.
-
-User Entitlements and Access Levels: Terraform supports basic user management but does not handle detailed user entitlements and access level adjustments beyond basic group memberships.
-
-Extension Management: Installing, configuring, and managing Azure DevOps extensions via the marketplace is not supported.
-
-Variable Group Permissions: While variable groups can be managed, specific permissions related to who can access or modify these groups are not covered.
-
-Environment Permissions: Management of environments for deployment pipelines, including setting specific permissions for these environments, is not supported.
-
 
 ========
 artifact feeds also use this RBAC role permission but with different, and more, roles.
