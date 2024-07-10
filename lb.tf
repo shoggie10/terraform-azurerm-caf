3797818cf42fbc98b47ab8ab3e4e9d7de850c402
shoggie10 <shoggie10@gmail.com>
2024-07-10 17:34:06 -0400

    Update lb.tf

--- lb.tf
+++ lb.tf
@@ -19,35 +19,7 @@ output "lb" {
   value = module.lb
 }
 ==========||========================
-Functionality	PA	LPA	Cloud	Comments/Justifications
-Team Configuration				
-Create child nodes		X	X	Enables structured team expansion and hierarchy management.
-Delete this node	X			Necessary for maintaining clean and relevant team structures.
-Edit this node	X	X		Essential for adapting team configurations to changing needs.
-View permissions for this node	X	X	X	Basic requirement to ensure appropriate access control.
-Dashboards				
-Delete dashboard	X			Critical for managing lifecycle and relevance of dashboards.
-Edit dashboard		X	X	Facilitates timely updates and personalization of dashboards.
-Manage permissions	X			Controls who can alter or view dashboard configurations.
-Wiki (Wiki Security)				
-Advanced Security: manage and dismiss alerts	X	X		Crucial for addressing and mitigating security alerts.
-Advanced Security: manage settings	X			Reserved for PAs to uphold stringent security standards.
-Advanced Security: view alerts		X	X	Allows timely response to security threats by monitoring alerts.
-Repository and Branch Policies				
-Bypass policies when completing pull requests	X			Empowers PAs to expedite changes in critical scenarios.
-Bypass policies when pushing	X			Enables PAs to implement urgent changes without delays.
-Contribute		X	X	Fundamental for ongoing development and project contributions.
-Contribute to pull requests		X	X	Supports collaborative review and enhancement of code.
-Create branch		X	X	Facilitates version control and feature development.
-Create tag		X	X	Useful for marking and referencing specific project states.
-Delete or disable repository	X			Essential for PA to manage repository lifecycle and security.
-Edit policies	X	X		Necessary for tailoring project policies to evolving needs.
-Force push (rewrite history, delete branches and tags)	X			Grants PAs the authority to correct or adjust project history.
-Manage notes		X	X	Useful for documenting important changes and decisions.
-Manage permissions	X			Critical for safeguarding access to project resources.
-Read		X	X	Basic access to participate in and understand project developments.
-Remove others' locks		X		Helps resolve code access conflicts, ensuring workflow continuity.
-Rename repository	X			Important for realigning project repositories with strategic goals.
+
 ========
 artifact feeds also use this RBAC role permission but with different, and more, roles.
 
