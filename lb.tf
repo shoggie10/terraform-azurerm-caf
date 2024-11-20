57ad7d30e93dd48a6c0d0d59fb6602e387695518
shoggie10 <shoggie10@gmail.com>
2024-11-19 22:32:47 -0500

    Update lb.tf

--- lb.tf
+++ lb.tf
@@ -21,154 +21,24 @@ output "lb" {
 ========================||========================
 # main.tf
 ---
-resource "azurerm_cosmosdb_sql_database" "this" {
-  name                = var.sql_database_name
-  account_name        = data.azurerm_cosmosdb_account.this.name
-  resource_group_name = var.resource_group_name
-}
-
-resource "azurerm_cosmosdb_sql_container" "this" {
-  name                = var.sql_container_name
-  resource_group_name = var.resource_group_name
-  account_name        = data.azurerm_cosmosdb_account.this.name
-  database_name       = azurerm_cosmosdb_sql_database.this.name
-
-  partition_key_path   = var.partition_key_path
-  throughput           = var.throughput
-
-  unique_key {
-    paths = var.unique_key_paths
-  }
-}
-
-module "rbac" {
-  source = "app.terraform.io/xxxx/common/azure"
-
-  for_each = var.role_assignments
-
-  resource_id   = azurerm_cosmosdb_sql_container.this.id
-  resource_name = azurerm_cosmosdb_sql_container.this.name
-
-  role_based_permissions = {
-    assignment = {
-      role_definition_id_or_name = each.value.role_definition_id_or_name
-      principal_id               = each.value.principal_id
-    }
-  }
-  wait_for_rbac = false
-}
 
 ###=======================================
 # globals.tf
 ---
-data "azurerm_client_config" "current" {}
 
-data "azurerm_cosmosdb_account" "this" {
-  name                = var.cosmosdb_account_name
-  resource_group_name = var.resource_group_name
-}
 
 ###=======================================
 # variables.tf
 ---
-variable "sql_database_name" {
-  description = "Name of the CosmosDB SQL database to create"
-  type        = string
-}
 
-variable "sql_container_name" {
-  description = "Name of the CosmosDB SQL container to create"
-  type        = string
-}
-
-variable "partition_key_path" {
-  description = "The partition key path for the CosmosDB SQL container"
-  type        = string
-}
-
-variable "throughput" {
-  description = "The throughput for the CosmosDB SQL container (e.g., RU/s)"
-  type        = number
-  default     = null
-}
-
-variable "unique_key_paths" {
-  description = "A list of unique key paths for the CosmosDB SQL container"
-  type        = list(string)
-  default     = []
-}
-
-variable "cosmosdb_account_name" {
-  description = "The name of the CosmosDB account"
-  type        = string
-}
-
-variable "resource_group_name" {
-  description = "The name of the resource group"
-  type        = string
-}
-
-variable "role_assignments" {
-  type = map(object({
-    role_definition_id_or_name = string
-    principal_id               = string
-  }))
-  default     = {}
-  description = <<DESCRIPTION
-A map of role assignments to create on the resource. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time.
-
-- `role_definition_id_or_name` - The ID or name of the role definition to assign to the principal.
-- `principal_id` - The ID of the principal to assign the role to.
-DESCRIPTION
-  nullable    = false
-}
-
-variable "terraform_module" {
-  description = "Used to inform of a parent module"
-  type        = string
-  default     = ""
-}
 
 
 ###=======================================
 # outputs.tf
 ---
-output "sql_database_id" {
-  value       = azurerm_cosmosdb_sql_database.this.id
-  description = "The ID of the CosmosDB SQL database"
-}
 
-output "sql_container_id" {
-  value       = azurerm_cosmosdb_sql_container.this.id
-  description = "The ID of the CosmosDB SQL container"
-}
-
-output "sql_database_name" {
-  value       = azurerm_cosmosdb_sql_database.this.id
-  description = "The name of the CosmosDB SQL database"
-}
-
-output "sql_container_name" {
-  value       = azurerm_cosmosdb_sql_container.this.name
-  description = "The name of the CosmosDB SQL container"
-}
 =====
-### Validation Blocks: Add validation blocks to ensure input variables meet certain criteria:
-variable "partition_key_path" {
-  description = "The partition key path for the CosmosDB SQL container"
-  type        = string
-  validation {
-    condition     = can(regex("^/.*", var.partition_key_path))
-    error_message = "The partition key path must start with a '/'."
-  }
-}
-### Dynamic Blocks for Unique Keys: If unique keys are optional, use dynamic blocks to conditionally include them:
-dynamic "unique_key" {
-  for_each = var.unique_key_paths
-  content {
-    paths = unique_key.value
-  }
-}
+
 
 ╵=============================||=================================================
 
