5fb1740fc170a900009a1b8138cbc804eab80e6c
shoggie10 <shoggie10@gmail.com>
2024-11-19 23:35:48 -0500

    Update lb.tf

--- lb.tf
+++ lb.tf
@@ -21,24 +21,141 @@ output "lb" {
 ========================||========================
 # main.tf
 ---
+resource "azurerm_cosmosdb_table" "this" {
+  name                = var.table_name
+  account_name        = data.azurerm_cosmosdb_account.this.name
+  resource_group_name = var.resource_group_name
+  throughput          = var.throughput
 
+  autoscale_settings {
+    max_throughput = var.max_throughput
+  }
+}
+
+module "rbac" {
+  source = "app.terraform.io/xxxx/common/azure"
+
+  for_each = var.role_assignments
+  depends_on = [azurerm_cosmosdb_table.this]
+
+  resource_id   = azurerm_cosmosdb_table.this.id
+  resource_name = azurerm_cosmosdb_table.this.name
+
+  role_based_permissions = {
+    assignment = {
+      role_definition_id_or_name = each.value.role_definition_id_or_name
+      principal_id               = each.value.principal_id
+    }
+  }
+  wait_for_rbac = false
+}
+---
+
+  dynamic "autoscale_settings" {
+    for_each = var.max_throughput != null ? [1] : []
+    content {
+      max_throughput = var.max_throughput
+    }
 ###=======================================
 # globals.tf
 ---
+data "azurerm_client_config" "current" {}
+
+data "azurerm_cosmosdb_account" "this" {
+  name                = var.cosmosdb_account_name
+  resource_group_name = var.resource_group_name
+}
 
 
 ###=======================================
 # variables.tf
 ---
+variable "table_name" {
+  description = "Name of the CosmosDB Table to create"
+  type        = string
+}
+
+variable "cosmosdb_account_name" {
+  description = "The name of the CosmosDB account"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "throughput" {
+  description = "The throughput for the CosmosDB Table (e.g., RU/s)"
+  type        = number
+  default     = null
+}
+
+variable "max_throughput" {
+  description = "The maximum throughput for autoscale settings"
+  type        = number
+  default     = null
+}
+
+variable "role_assignments" {
+  type = map(object({
+    role_definition_id_or_name = string
+    principal_id               = string
+  }))
+  default     = {}
+  description = <<DESCRIPTION
+A map of role assignments to create on the resource. The map key is deliberately arbitrary to avoid issues where map keys may be unknown at plan time.
+
+- `role_definition_id_or_name` - The ID or name of the role definition to assign to the principal.
+- `principal_id` - The ID of the principal to assign the role to.
+DESCRIPTION
+  nullable    = false
+}
+
+variable "terraform_module" {
+  description = "Used to inform of a parent module"
+  type        = string
+  default     = ""
+}
 
 
 
 ###=======================================
 # outputs.tf
 ---
+output "cosmosdb_table_id" {
+  value       = azurerm_cosmosdb_table.this.id
+  description = "The ID of the CosmosDB Table"
+}
+
+output "cosmosdb_table_name" {
+  value       = azurerm_cosmosdb_table.this.name
+  description = "The name of the CosmosDB Table"
+}
+---
+output "cosmosdb_table_resource_id" {
+  value       = azurerm_cosmosdb_table.this.id
+  description = "The resource ID of the CosmosDB Table"
+}
+
+output "cosmosdb_table_etag" {
+  value       = azurerm_cosmosdb_table.this.etag
+  description = "The ETag of the CosmosDB Table"
+}
 
 =====
+locals {
+  throughput_settings = [var.throughput, var.max_throughput]
+}
 
+variable "throughput_validation" {
+  description = "Validates that only one of throughput or max_throughput is set"
+  default     = null
+  validation {
+    condition     = length(compact(local.throughput_settings)) == 1
+    error_message = "You must specify either 'throughput' or 'max_throughput', not both."
+  }
+}
 
 ╵=============================||=================================================
 
