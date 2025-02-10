284203f3cd511b411485742fb7aa66702a8da689
shoggie10 <shoggie10@gmail.com>
2025-02-09 23:16:29 -0500

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cosmosdb_table_id" {
-  description = "The ID of the Cosmos DB Table."
-  value       = azurerm_cosmosdb_table.this.id
-}
-
-output "cosmosdb_table_name" {
-  description = "The ID of the Cosmos DB Table."
-  value       = azurerm_cosmosdb_table.this.name
-}
