c754b366019524ff97c7a69b6c2000016600f270
shoggie10 <shoggie10@gmail.com>
2025-02-09 23:12:28 -0500

    Delete versions.tf

--- versions.tf
+++
@@ -1,9 +0,0 @@
-## Please refer to version template document for setting this configuration.
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "< 5.0.0"
-    }
-  }
-}
